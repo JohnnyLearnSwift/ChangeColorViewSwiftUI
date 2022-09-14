ac5570901e6ea0c5aef048aa2693040b3ec5a79c
JohnyLearnSwift <eugenietyan@gmail.com>
2022-09-14 12:29:06 +0300

    Initial Commit

---
+++ ChangeColorViewSwiftUI/ChangeColorViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeColorViewSwiftUIApp.swift
+//  ChangeColorViewSwiftUI
+//
+//  Created by Eugenie Tyan on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChangeColorViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
